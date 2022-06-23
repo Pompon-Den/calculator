9ac3e6f954fc66be206e7f84e4b859154d70baea
pompon <danil.ryzhkov.03@list.ru>
2022-06-23 17:35:34 +1000

    первый коммит

---
+++ MegaCalculator/MegaCalculator/MainWindow.xaml.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MegaCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Pro1 calc = new Pro1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (calc.Ban_Button == false)
+            {
+                if (calc.Correct_Enter == false)
+                {
+                    Box.Text = "";
+                    calc.Correct_Enter = true;
+                }
+                Box.PreviewTextInput += new TextCompositionEventHandler(Box_PreviewTextInput);
+                //если есть знак "бесконечность" - не даёт писать цифры после него
+                if (Box.Text.IndexOf("∞") != -1)
+                {
+                    Box.Text = Box.Text.Substring(0, Box.Text.Length - 1);
+                }
+                //ситуация: слева ноль, а после него НЕ запятая, тогда ноль можно удалить
+                if (Box.Text[0] == '0' && (Box.Text.IndexOf(",") != 1) && Box.Text.Length > 1)
+                {
+                    Box.Text = Box.Text.Remove(0, 1);
+                }
+                History_Box.Text = Box.Text;
+            }
+        }
+
+        private void But_Click(object sender, RoutedEventArgs e)
+        {
+            if (calc.Ban_Button == false)
+            {
+                if (calc.Correct_Enter == false)
+                {
+                    Box.Text = "";
+                    calc.Correct_Enter = true;
+                }
+                Box.Text = Box.Text + (sender as Button).Content;
+
+                //если есть знак "бесконечность" - не даёт писать цифры после него
+                if (Box.Text.IndexOf("∞") != -1)
+                {
+                    Box.Text = Box.Text.Substring(0, Box.Text.Length - 1);
+                }
+                //ситуация: слева ноль, а после него НЕ запятая, тогда ноль можно удалить
+                if (Box.Text[0] == '0' && (Box.Text.IndexOf(",") != 1) && Box.Text.Length > 1)
+                {
+                    Box.Text = Box.Text.Remove(0, 1);
+                }
+                
+                calc.History += (sender as Button).Content; 
+                History_Box.Text = calc.History;
+            }
+        }
+
+        private void But_Plus(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Plus();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+
+        private void But_Minus(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Minus();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+
+        private void But_Ravno(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Ravno();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+        private void But_Multiply(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Multiply();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+        private void But_Divide(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Divide();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+
+        private void But_Reverse(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Revers();
+            Box.Text = calc.Work_String;
+        }
+
+        private void But_ChangeSign(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.ChangeSign();
+            Box.Text = calc.Work_String;
+        }
+
+        private void But_Point(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Point();
+            Box.Text = calc.Work_String;
+        }
+
+        private void But_Sqrt(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.Sqrt_Two();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+
+        private void But_C(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.C();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+        private void But_CE(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.CE();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+        private void But_Delete_Last(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.DeleteLastChar();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+        }
+
+        private void But_MS(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.MSave_Number();
+            Memory_Box.Text = "M";
+        }
+        private void But_MR(object sender, RoutedEventArgs e)
+        {
+            
+            calc.MR();
+            Box.Text = calc.Work_String;
+            History_Box.Text = calc.History;
+
+        }
+        private void But_MC(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.MC();
+            Memory_Box.Text = "";
+            Box.Text = calc.Work_String;
+        }
+        private void But_M_Plus(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.MPlus();
+        }
+        private void But_M_Mimus(object sender, RoutedEventArgs e)
+        {
+            calc.Work_String = Box.Text;
+            calc.MMinus();
+        }
+
+        private void Box_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Multiply)
+            {
+                calc.Work_String = Box.Text;
+                calc.Multiply();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Divide)
+            {
+                calc.Work_String = Box.Text;
+                calc.Divide();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Subtract)
+            {
+                calc.Work_String = Box.Text;
+                calc.Minus();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Add)
+            {
+                calc.Work_String = Box.Text;
+                calc.Plus();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Multiply)
+            {
+                calc.Work_String = Box.Text;
+                calc.Multiply();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Divide)
+            {
+                calc.Work_String = Box.Text;
+                calc.Divide();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Subtract)
+            {
+                calc.Work_String = Box.Text;
+                calc.Minus();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+            if (e.Key == Key.Add)
+            {
+                calc.Work_String = Box.Text;
+                calc.Plus();
+                Box.Text = calc.Work_String;
+                History_Box.Text = calc.History;
+            }
+
+        }
+    }
+}
---
+++ MegaCalculator/MegaCalculator/Pro1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCalculator
+{
+   public class Pro1
+    {
+        public double Answer { get; private set; }
+        public string Work_String, History = "";
+        public double Operand;
+        public enum State { FirstOp, SecondOp };
+        public State state = State.FirstOp;
+        public bool Correct_Enter = true; 
+        public bool Ban_Button = false;
+        private double memory = 0;
+        private bool Memory_Clear = true;
+        public enum Action {  pusto, plus, minus, multiply, divide };
+        public Action action = Action.pusto;
+
+        public void MPlus() //кнопка М+
+        {
+            if (Memory_Clear == false)
+            {
+                memory += double.Parse(Work_String);
+            }
+        }
+       
+        public void MMinus() //кнопка М-
+        {
+            if (Memory_Clear == false)
+            {
+                memory -= double.Parse(Work_String);
+            }
+        }
+
+        public void MSave_Number() //кнопка МS сохранить
+        {
+            memory = double.Parse(Work_String);
+            Memory_Clear = false;
+        }
+   
+        public void MC() //кнопка МC/ удалить
+        {
+            memory = 0;
+            Memory_Clear = true;
+        }
+        public void MR() //кнопка МR/ показать
+        {
+            if (Memory_Clear == false)
+            {
+                Work_String = memory.ToString();
+            }
+        }
+        public void Plus()
+        {
+            if (Ban_Button == false)
+            {
+                History += " + ";
+                Correct_Enter = false;
+
+                if (state == State.FirstOp)
+                {
+                    Answer = double.Parse(Work_String);
+                    state = State.SecondOp;
+                    
+                }
+                else
+                {
+                    Operand = double.Parse(Work_String);
+                    HelpProcess();
+                    
+                  
+                }
+                action = Action.plus;
+            }
+        }
+
+        public void HelpProcess()
+        {
+            if (action == Action.plus)
+            {
+                Answer += Operand;
+                Work_String = Answer.ToString();
+            }
+            if (action == Action.minus)
+            {
+                Answer -= Operand;
+                Work_String = Answer.ToString();
+            }
+            if (action == Action.multiply)
+            {
+                Answer *= Operand;
+                Work_String = Answer.ToString();
+            }
+            if (action == Action.divide)
+            {
+                if (Operand != 0)
+                {
+                    Answer /= Operand;
+                    Work_String = Answer.ToString();
+                }
+                else
+                {
+                    Work_String = "Деление на 0 невозможно";
+                    Ban_Button = true;
+                }
+            }
+        }
+        public void Minus()
+        {
+            if (Ban_Button == false)
+            {
+                History += " - ";
+                Correct_Enter = false;
+
+                if (state == State.FirstOp)
+                {
+                    Answer = double.Parse(Work_String);
+                    state = State.SecondOp;
+                    
+                }
+                else
+                {
+                    Operand = double.Parse(Work_String);
+                    HelpProcess();
+                    Work_String = Answer.ToString();
+                    
+                }
+                action = Action.minus;
+            }
+        }
+
+        public void Multiply()
+        {
+            if (Ban_Button == false)
+            {
+                History += " * ";
+                Correct_Enter = false;
+
+                if (state == State.FirstOp)
+                {
+                    Answer = double.Parse(Work_String);
+                    state = State.SecondOp;
+                    
+                }
+                else
+                {
+                    Operand = double.Parse(Work_String);
+                    HelpProcess();
+                    Work_String = Answer.ToString();
+                }
+                action = Action.multiply;
+            }
+        }
+
+        public void Divide()
+        {
+            if (Ban_Button == false)
+            {
+                History += " / ";
+                Correct_Enter = false;
+
+                if (state == State.FirstOp)
+                {
+                    Answer = double.Parse(Work_String);
+                    state = State.SecondOp;
+                    
+                }
+                else
+                {
+                    Operand = double.Parse(Work_String);
+                    HelpProcess();
+                    Work_String = Answer.ToString();
+                }
+                action = Action.divide;
+            }
+        }
+    
+        public void Revers()
+        {
+            if (Ban_Button == false)
+            {
+                Answer = double.Parse(Work_String);
+                if ( Answer != 0)
+                {
+                    Answer = 1 / Answer;
+                    Work_String = Answer.ToString();
+                }
+                else
+                {
+                    Work_String = "Деление на 0 невозможно";
+                    Ban_Button = true;
+                }
+            }
+        }
+
+        public void ChangeSign()
+        {
+            if (Ban_Button == false)
+            {
+                if (double.Parse(Work_String) != 0)
+                {
+                    Work_String = Work_String[0] == '-' ? Work_String.Remove(0, 1) : "-" + Work_String;
+                }
+            }
+        }
+
+       
+        public void Sqrt_Two()
+        {
+            if (Ban_Button == false)
+            {
+                Answer = double.Parse(Work_String);
+                History = " sqrt(" + Work_String + ")";
+                if (Answer >= 0)
+                {
+                    Answer = Math.Sqrt(Answer);
+                    Work_String = Answer.ToString();
+                }
+                else
+                {
+                    Work_String = "Недопустимый ввод";
+                    Ban_Button = true;
+
+                }
+            }
+        }
+
+        public void DeleteLastChar()
+        {
+            if (Work_String.Length > 1 && Work_String.Contains("E")  == false )
+            {
+                Work_String = Work_String.Remove(Work_String.Length - 1);
+                History = Work_String;
+            }
+            else
+            {
+                Work_String = "0";
+                History = "";
+            }
+        }
+        public void C()
+        {
+            Ban_Button = false;
+            Work_String = "0";
+            History = "";
+        }
+
+        public void CE()
+        {
+            Ban_Button = false;
+            Work_String = "0";
+        }
+
+        public void Point()
+        {
+            if (Ban_Button == false)
+            {
+                if ((Work_String.IndexOf(",") == -1) && (Work_String.IndexOf("∞") == -1))
+                {
+                    Work_String += ",";
+                }
+            }
+        }
+        public void Ravno()
+        {
+            if (Ban_Button == false)
+            {
+                
+               Operand = double.Parse(Work_String);
+                Correct_Enter = false;
+
+                if (action == Action.pusto) { }
+                if (action == Action.plus)
+                {
+                    Answer += Operand;
+                    Work_String = Answer.ToString();
+                    state = State.FirstOp;
+                }
+                if (action == Action.minus)
+                {
+                    Answer -= Operand;
+                    Work_String = Answer.ToString();
+                    state = State.FirstOp;
+                }
+                if (action == Action.multiply)
+                {
+                    Answer *= Operand;
+                    Work_String = Answer.ToString();
+                    state = State.FirstOp;
+                }
+                if (action == Action.divide)
+                {
+                    if (Operand != 0)
+                    {
+                        Answer /= Operand;
+                        Work_String = Answer.ToString();
+                    }
+                    else
+                    {
+                        Work_String = "Деление на 0 невозможно";
+                        Ban_Button = true;
+                    }
+                    state = State.FirstOp;
+                }
+
+                History = "";
+            }
+        }
+
+
+
+    }
+}
